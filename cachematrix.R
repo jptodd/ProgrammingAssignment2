edf094058e3ff4fc8fdc91584a10e7ddfafcecc7
jptodd <jptodd21@gmail.com>
2015-07-25 20:32:43 -0700

    First commit with assignment and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming Assignment 2
+## 
 
-## Write a short comment describing this function
+## Use lexical scoping and <<- operator to create a set of functions
+## that convert a matrix to a special matrix which caches the iverse 
+## after it is solved the first time so that performance is improved if 
+## the inverse is required multiple times
+
+
+## makeCacheMatrix accepts a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        message("in makeCacheMatrix$set")
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(m) inv <<- m
+    getInverse <- function() inv
+    list(set = set, get = get, 
+         setInverse = setInverse, getInverse = getInverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns the invers of a matrix created with makeCacheMatrix
+## first time it is called with the matrix, it caculates it and saves it before 
+## returning the result.  Subsequent calls use the saved results
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if (!is.null(i)){
+        message("using saved inverse")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
+    
 }
